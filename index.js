5c4556346cfa205ca6fcdcf186edf650dc481448
rejaul <rejaulvoice@gmail.com>
2024-12-08 01:03:32 +0000

    findout the error and solved it

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-// const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // middleware
