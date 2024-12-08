206034618169cf434a325425a8ba42cbddecfd29
rejaul <rejaulvoice@gmail.com>
2024-12-08 00:44:36 +0000

    I have added deled api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -24,21 +24,69 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     //Sports equipment database creation
     const sportsCollection = client.db('equipmentDB').collection('equipment')
 
+    // data getting api
+    app.get('/sports', async (req, res) => {
+      const cursor = sportsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // getting single data api
+    app.get('/sports/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await sportsCollection.findOne(query);
+      res.send(result);
+    })
+
     // data creating api
-    app.post('/sports', async(req, res)=>{
+    app.post('/sports', async (req, res) => {
       const newEquip = req.body;
-      console.log(newEquip);
       const result = await sportsCollection.insertOne(newEquip);
       res.send(result);
     })
 
+    // updating equipments data
+    app.put('/sports/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedEquip = req.body;
+
+      const equip = {
+        $set: {
+          itemName: updatedEquip.itemName,
+          category: updatedEquip.category,
+          description: updatedEquip.description,
+          price: updatedEquip.price,
+          rating: updatedEquip.rating,
+          customization: updatedEquip.customization,
+          processTime: updatedEquip.processTime,
+          stock: updatedEquip.stock,
+          userEmail: updatedEquip.userEmail,
+          userName: updatedEquip.userName,
+          equipmentPhoto: updatedEquip.equipmentPhoto,
+        }
+      }
+      const result = await sportsCollection.updateOne(filter, equip, options)
+      res.send(result);
+    })
+
+     // Delete route
+     app.delete('/sports/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      result = await sportsCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -47,10 +95,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
-    res.send('equisports-lisboa-server is running');
+app.get('/', (req, res) => {
+  res.send('equisports-lisboa-server is running');
 })
 
-app.listen(port, ()=>{
-    console.log(`equisports-lisboa is running on port: ${port}`);
+app.listen(port, () => {
 })
